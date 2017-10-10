4af2b70a4859515dc2bfab206240a1952bf8f710
monse Tolentino <monse.tolentino@live.com>
2017-10-10 14:02:24 -0500

    se afrego logica

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-console.log ("hola");
-
+/*
 var nombre = "monse";
 var nick = "azul";
 
-console.log(nombre, "es", nick);
+console.log(nombre, "es", nick);*/
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
