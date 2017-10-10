4f890a4da4fefe26e226e4a365fcafabe45781e7
monse Tolentino <monse.tolentino@live.com>
2017-10-10 14:07:50 -0500

    se modifico la forma de acceso

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ console.log(nombre, "es", nick);*/
 const express = require('express')
 const app = express()
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
-})
+});
